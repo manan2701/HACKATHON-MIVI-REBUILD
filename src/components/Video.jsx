ece7afa040f113f713725704d18f810b2ca82164
manan2701 <mananborda3@gmail.com>
2025-07-18 18:25:52 +0530

    sec

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -12,7 +12,7 @@ const Video = () => {
         autoPlay
         loop
         muted
-        src="./src/assets/videon2.mp4"
+        src="https://www.mivi.in/cdn/shop/videos/c/vp/01eb3a9ba85b4a74a7aaef16b765376a/01eb3a9ba85b4a74a7aaef16b765376a.SD-480p-1.5Mbps-50069298.mp4?v=0"
       ></video>
     </div>
   );
